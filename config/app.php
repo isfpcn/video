a101f90244380dcd4e3a3de6a96f66ebc62995e2
qingsongyao <454919140@qq.com>
2016-08-25 15:32:13 +0800

    wechat config

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Overtrue\LaravelWechat\ServiceProvider::class,
 
     ],
 
@@ -208,6 +209,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Wechat' => Overtrue\LaravelWechat\Facade::class,
 
     ],
 
